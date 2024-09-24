7863af81d18c1a82fdfeacdeb4ec338bc8c6c792
mobo07 <mikemobo900@gmail.com>
2024-09-24 10:51:14 +0100

    initial commit

---
+++ js/index.js
@@ -0,0 +1,93 @@
+import { locations } from "./locations.js";
+
+const dropdownHeader = document.querySelector(".dropdown-header");
+const dropdownList = document.querySelector(".dropdown-list");
+
+dropdownHeader.addEventListener("click", () => {
+  dropdownList.classList.toggle("show");
+});
+
+const binIcon = L.icon({
+  iconUrl: "../public/assets/images/bin.png",
+
+  iconSize: [35, 35], // size of the icon
+  iconAnchor: [20, 20], // point of the icon which will correspond to marker's location
+  popupAnchor: [-1, -20], // point from which the popup should open relative to the iconAnchor
+});
+const recycleIcon = L.icon({
+  iconUrl: "../public/assets/images/recycle-symbol.png",
+
+  iconSize: [35, 35], // size of the icon
+  iconAnchor: [20, 20], // point of the icon which will correspond to marker's location
+  popupAnchor: [-1, -20], // point from which the popup should open relative to the iconAnchor
+});
+const incinerateIcon = L.icon({
+  iconUrl: "../public/assets/images/incineration.png",
+
+  iconSize: [35, 35], // size of the icon
+  iconAnchor: [20, 20], // point of the icon which will correspond to marker's location
+  popupAnchor: [-5, -20], // point from which the popup should open relative to the iconAnchor
+});
+
+// const map = L.map("map", {
+//   center: [9.84731188879364, 8.900734931230545],
+//   zoom: 13,
+// });
+const map = L.map("map").setView([9.84731188879364, 8.900734931230545], 13);
+
+const createListItem = (content) => {
+  const li = document.createElement("li");
+  li.innerText = content;
+  li.classList.add("list-item");
+  li.addEventListener("click", () => {
+    const coords = locations.filter(
+      (location) => location.street === content
+    )[0];
+    map.setView([coords.lat, coords.lng], 17);
+    dropdownList.classList.remove("show");
+  });
+  return li;
+};
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+locations.forEach((location) => {
+  const li = createListItem(location.street);
+  dropdownList.appendChild(li);
+  if (location.marker === "waste-bin")
+    L.marker([location.lat, location.lng], { icon: binIcon })
+      .bindPopup(location.street)
+      .addTo(map);
+  else if (location.marker === "recycle")
+    L.marker([location.lat, location.lng], { icon: recycleIcon })
+      .bindPopup(location.street)
+      .addTo(map);
+  else if (location.marker === "incinerator")
+    L.marker([location.lat, location.lng], { icon: incinerateIcon })
+      .bindPopup(location.street)
+      .addTo(map);
+  else
+    L.marker([location.lat, location.lng])
+      .bindPopup(location.street)
+      .addTo(map);
+});
+
+// const binIcon = L.icon({
+//   iconUrl: "../public/assets/images/recycle-symbol.png",
+
+//   iconSize: [35, 35], // size of the icon
+//   iconAnchor: [20, 20], // point of the icon which will correspond to marker's location
+//   popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+// });
+
+// const map = L.map("map").setView([51.505, -0.09], 13);
+
+// L.marker([9.861659082374654, 8.85862223803997], {
+//   icon: binIcon,
+// }).addTo(map);
+
+// var marker = L.marker([51.5, -0.09]).addTo(map);
---
+++ js/locations.js
@@ -0,0 +1,62 @@
+export const locations = [
+  {
+    street: "Barkin Ladi Rd",
+    lat: 9.84731188879364,
+    lng: 8.900734931230545,
+    marker: "default",
+  },
+  {
+    street: "Miango Rd",
+    lat: 9.863022319023768,
+    lng: 8.866327553987503,
+    marker: "default",
+  },
+  {
+    street: "A M Dung St, Dadinkowa",
+    lat: 9.858631005416031,
+    lng: 8.866145834326744,
+    marker: "default",
+  },
+  {
+    street: "Farin Gada Rd",
+    lat: 9.957686207839135,
+    lng: 8.864655531942844,
+    marker: "default",
+  },
+  {
+    street: "Ignatius Durlong St",
+    lat: 9.869555008183776,
+    lng: 8.84883251041174,
+    marker: "recycle",
+  },
+  {
+    street: "Jos 930104, Plateau",
+    lat: 9.861659082374654,
+    lng: 8.85862223803997,
+    marker: "waste-bin",
+  },
+  {
+    street: "Pavina Global Service Ltd No 3 Pam Bot Street Hwolshe",
+    lat: 9.882210093735203,
+    lng: 8.874041587114334,
+    marker: "recycle",
+  },
+  {
+    street: "35 Yakubu Gowon Way, Jos",
+    lat: 9.882220663383718,
+    lng: 8.874507620930672,
+    marker: "waste-bin",
+  },
+  {
+    street: "Abbattoir market, adjacent Admin Block",
+    lat: 9.8855668,
+    lng: 8.887848500000002,
+    marker: "incinerator",
+  },
+  //   {
+  //     street: "Kamasin 3mtt Ux",
+  //     lat: 9.8855668,
+  //     lng: 8.887848500000002,
+  //     marker: "recycle",
+  //   },
+];
